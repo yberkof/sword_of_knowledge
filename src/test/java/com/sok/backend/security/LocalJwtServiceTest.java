5401090eb07c1ce080d91e5d6248779af26bc4cf
YO07 <yahia19998120@gmail.com>
2026-05-19 01:05:05 +0300

    stable-ish

--- src/test/java/com/sok/backend/security/LocalJwtServiceTest.java
+++ src/test/java/com/sok/backend/security/LocalJwtServiceTest.java
@@ -8,7 +8,7 @@ class LocalJwtServiceTest {
   void accessTokenRoundTripWorks() {
     LocalJwtService svc = new LocalJwtService("test_secret_123456789012345678901234", "issuer", 300, 600);
     String token = svc.issueAccessToken("u1", java.util.UUID.randomUUID());
-    LocalJwtService.LocalClaims claims = svc.verifyAccessToken(token);
+    LocalClaims claims = svc.verifyAccessToken(token);
     Assertions.assertEquals("u1", claims.userId());
   }
 
